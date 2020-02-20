2c553e09056714b5565204384728efe67925c2a7
Andy Marshall <ajmarshall@mail.com>
2020-02-20 20:35:07 +0000

    Delete marshmallow_footer.php

--- inc/marshmallow_footer.php
+++
@@ -1,8 +0,0 @@
-<?php echo '<!-- FOOTER -->
-	<div class="container">  
-	  <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2017 Marshmallow web design Ltd &middot;</p>
-      </footer> 
-	  </div>';
-?>
