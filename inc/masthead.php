a301e297675b622a6939d3700c20050e0a2eb64d
Andy Marshall <ajmarshall@mail.com>
2020-02-20 22:08:45 +0000

    Update masthead.php

--- inc/masthead.php
+++ inc/masthead.php
@@ -3,8 +3,8 @@
  <br>
  <div class="row">
  <div class="col-md-3">
- <img src="../Images/Logo.png"
- style="width:160px;height:200px" alt="Mrs Marshmallow logo">
+ <img-fluid src="../Images/Logo.png"
+ style="width:100%;height:auto" alt="Mrs Marshmallow logo">
  </div>
  <div class="col-md-9">
  <h1 style="margin-top: 140px; margin-bottom: 0px">Cakes, Cupcakes and Balloons</h1>
