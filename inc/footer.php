d34dccec5eb21cdefb82cfbe031183ce66e1ab95
Andy Marshall <ajmarshall@mail.com>
2021-04-05 20:30:01 +0100

    Delete footer.php

--- inc/footer.php
+++
@@ -1,8 +0,0 @@
-<?php echo '<!-- FOOTER -->
-	<div class="container">  
-	  <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2016 AJM Services Ltd &middot;</p>
-      </footer> 
-	  </div>';
-?>
